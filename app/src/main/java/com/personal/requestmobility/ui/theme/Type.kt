8b18f6eace5a332a9866ed9690a5475b33f023f6
danielgomezpascual <dgomezp.ext@maxamcorp.com>
2025-07-11 14:02:16 +0200

    definicion de fuentes

--- app/src/main/java/com/personal/requestmobility/ui/theme/Type.kt
+++ app/src/main/java/com/personal/requestmobility/ui/theme/Type.kt
@@ -2,14 +2,37 @@ package com.personal.requestmobility.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.personal.requestmobility.R
 
-// Set of Material typography styles to start with
+// Set of
+//
+// Material typography styles to start with
+
+
+
+// Define la familia de fuentes agrupando sus diferentes pesos
+val Fira = FontFamily(
+    Font(R.font.fira, FontWeight.Normal),
+    Font(R.font.fira, FontWeight.Bold)
+    // Puedes añadir más aquí: Italic, Light, Medium, etc.
+    // Font(R.font.rubik_italic, FontWeight.Normal, FontStyle.Italic)
+)
+val Sira = FontFamily(
+    Font(R.font.sira, FontWeight.Normal),
+    Font(R.font.sira_sbold, FontWeight.Bold)
+    // Puedes añadir más aquí: Italic, Light, Medium, etc.
+    // Font(R.font.rubik_italic, FontWeight.Normal, FontStyle.Italic)
+)
+
+
+
 val Typography = Typography(
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fira,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
@@ -18,7 +41,7 @@ val Typography = Typography(
 
 
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fira,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
