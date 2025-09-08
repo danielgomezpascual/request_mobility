c9994f50c59d51879b33d9e63576e8bef00f061d
danielgomezpascual <dgomezp.ext@maxamcorp.com>
2025-09-08 10:30:26 +0200

    cwentradr el contenido del panel de la tabla.

--- app/src/main/java/com/personal/metricas/core/composables/listas/MA_NoData.kt
+++ app/src/main/java/com/personal/metricas/core/composables/listas/MA_NoData.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.personal.metricas.R
+import com.personal.metricas.core.composables.MA_Spacer
 import com.personal.metricas.core.composables.imagenes.MA_ImagenDrawable
 import com.personal.metricas.core.composables.labels.MA_LabelNormal
 import com.personal.metricas.core.utils._t
@@ -22,6 +23,7 @@ fun MA_NoData(){
 	Row(modifier = Modifier.fillMaxSize(),
 		horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
 		MA_ImagenDrawable(R.drawable.no_data, s = 48.dp)
+		MA_Spacer()
 		MA_LabelNormal(modifier = Modifier.fillMaxWidth(), valor = _t (R.string.sin_informacion))
 	}
 }
